a61e4448c084d23456311fb22382bff299563f4a
Sharma Natvar <krish997854@gmail.com>
2023-11-10 06:40:51 +0530

    add folder

---
+++ day19_js/index.js
@@ -0,0 +1,18 @@
+let nameLast = "krishna";
+console.log(`hay ${nameLast}`);
+
+
+
+console.log(1 + '1' - 1); 
+
+function foo() {
+    console.log(a);
+}
+  
+function bar() {
+    var a = 3;
+    foo();
+}
+
+var a = 5;
+bar();
