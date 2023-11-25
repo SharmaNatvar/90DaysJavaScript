8e583e8c9ee0a9228e834113999cc134c331d04c
Sharma Natvar <krish997854@gmail.com>
2023-11-25 21:36:23 +0530

    code day 5

---
+++ day5/script1.js
@@ -0,0 +1,9 @@
+// wap to print all element of arrat using for loop
+
+const arr = [1,2,3,4,5,6];
+
+for(let i=0; i < arr.length; i++){
+    console.log('arr'+'['+i+']'+' = '+arr[i]);
+}
+
+console.log(i);
