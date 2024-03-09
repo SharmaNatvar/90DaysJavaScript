a8af45aa2183ecd5c8494b3c90edad9f5a8a19a6
Sharma Natvar <krish997854@gmail.com>
2024-03-09 17:14:51 +0530

    add file

---
+++ day35_js/script.js
@@ -0,0 +1,46 @@
+// "use strict"
+// function a(){
+//     // return a = 9
+// }
+// var b = a()
+// console.log(b);
+
+// {
+//     let b = 10
+// }
+
+// console.log(b);
+// document.ge
+
+// var a= 10
+// var a= 'abhi'
+
+// console.log(a);
+
+// var a = 10
+
+// if(function abc(){}){
+//     a += typeof abc
+// }
+
+// console.log(a);
+
+
+// var student = {
+//     collage : 'abc'
+// }
+
+// console.log(student ,'stud0');
+// var stud1 = Object.create(student)
+// console.log(stud1 ,'stud1');
+// delete stud1.collage
+// console.log(stud1 ,'stud2');
+// console.log(stud1.company);
+
+
+// var Bar = Function Foo()
+// {
+//     return 11
+// }
+
+// typeof Foo()
